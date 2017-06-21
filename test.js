2c04583b16de9244c169e3a1ce7c4e20708c9b68
Antoni <antoni.oliver.tomas@gmail.com>
2017-06-22 01:12:44 +0200

    Batched

--- test.js
+++ test.js
@@ -3,15 +3,57 @@ const path = require('path');
 const request = require('request-promise-native');
 const jimp = require('jimp');
 
-function processGroup(all) {
-    console.log('All for group' + all[0].group);
+const GROUP_SIZE = 100;
+const IMAGE_SIZE = 27;
 
-    let image = new jimp(1080, 1080, (err, image) => {
-        all.forEach(i => {
-            console.log('blit', i.pos);
-            image.blit(i.image, i.pos.x * 108, i.pos.y * 108);
+function schedule(order, group) {
+    console.log('schedule', group);
+    let promises = [];
+
+    let keys = Object.keys(order).filter(i => {
+        return i >= group * GROUP_SIZE && i < (group + 1) * GROUP_SIZE;
+    });
+
+    keys.forEach(order_id => {
+
+        let rem = order_id % 100;
+        let pos = {
+            x: rem % 10,
+            y: Math.floor(rem / 10)
+        };
+
+        promises.push(jimp.read('http://2.cdn.bravefrontier.gumi.sg/content/unit/img/unit_ills_battle_' + order[order_id] + '.png').then((image) => {
+            console.log(order_id, 'received');
+            return {
+                image: image,
+                pos: pos
+            }
+        }).catch(error => {
+            console.error('Error on image#' + order_id);
+            console.error(error);
+        }));
+    });
+
+    Promise.all(promises).then(all => {
+        new jimp(IMAGE_SIZE * 10, IMAGE_SIZE * 10, (err, image) => {
+            all.forEach(i => {
+                console.log('blit', i.pos);
+                image.blit(i.image.scaleToFit(IMAGE_SIZE, IMAGE_SIZE), i.pos.x * IMAGE_SIZE, i.pos.y * IMAGE_SIZE);
+            });
+            console.log('saving', group);
+            image.write(path.join('img', group + '.png'), (err) => {
+                let remainingKeys = Object.keys(order).filter(i => {
+                    return i >= (group + 1) * 100;
+                });
+
+                if (remainingKeys.length) {
+                    let nextGroup = Math.floor(remainingKeys[0] / GROUP_SIZE);
+                    process.nextTick(schedule, order, nextGroup);
+                }
+            });
         });
-        image.write(path.join('img', all[0].group + '.png'));
+    }).catch(err => {
+        console.error(err);
     });
 }
 
@@ -27,72 +69,19 @@ fs.readFile(path.join('bravefrontier_data', 'info.json'), 'utf8', (err, data) =>
     let order = {};
 
     for (let id in info) {
+        // TODO units with alternate art!
         let unit = info[id];
-        /*order[unit.guide_id] = {
-            id: id,
-            name: unit.name,
-            rarity: unit.rarity
-        };*/
-        order[unit.guide_id] = id;
-    }
-
-    let lastGroup = -1;
-    let promises = [];
-
-    for (let order_id in order) {
-
-        let unitId = order[order_id];
-        let idMinus1 = order_id - 1;
-
-        let group = Math.floor(idMinus1 / 100);
-
-        if (group !== lastGroup) {
-            //TODO save image
-            if (lastGroup !== -1) {
-                Promise.all(promises[lastGroup]).then(processGroup).catch((all) => {
-                    console.error(all);
-                });
-            }
-            console.log('Guardant darrer grup: ', lastGroup);
-            //TODO create image
-            lastGroup = group;
-            promises[group] = [];
+        if (!order[unit.guide_id]) {
+            // There are some duplicated guide_ids...
+            order[unit.guide_id] = id;
         }
-
-        let rem = idMinus1 % 100;
-
-        let pos = {
-            x: rem % 10,
-            y: Math.floor(rem / 10)
-        };
-
-        promises[group].push(jimp.read('http://2.cdn.bravefrontier.gumi.sg/content/unit/img/unit_ills_battle_' + unitId + '.png').then((image) => {
-            return {
-                image: image,
-                pos: pos,
-                group: group
-            }
-        }).catch(error => {
-            console.error('Error on image#' + idMinus1);
-            console.error(error);
-        }));
-
-
-        //TODO get and save image
-        //TODO async: await all promises or whatever from a group, then save
-        console.log('Afegint ', order_id, ' a ', group, ' posició: ', pos);
     }
-
-    //TODO save image
-    console.log('Acabat. Guardant darrer grup: ', lastGroup);
-    Promise.all(promises[lastGroup]).then(processGroup).catch((all) => {
-        console.error(all);
-    });
-
 
     fs.writeFile('order.json', JSON.stringify(order, null, 4), (err) => {
         if (err) {
             console.error(err);
         }
     });
+
+    schedule(order, 0);
 });
