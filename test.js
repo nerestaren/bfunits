4a301696b79eaa631c64b0881ff26db01b11ff16
Antoni Oliver Tomàs <antoni.oliver.tomas@gmail.com>
2017-06-21 14:33:07 +0200

    ja faig imatges!! :D
    Però el puto id es veu que es comparteix :S Adjutant Mei i Maxwell Omni :S

--- test.js
+++ test.js
@@ -1,5 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const request = require('request-promise-native');
+const jimp = require('jimp');
+
+function processGroup(all) {
+    console.log('All for group' + all[0].group);
+
+    let image = new jimp(1080, 1080, (err, image) => {
+        all.forEach(i => {
+            console.log('blit', i.pos);
+            image.blit(i.image, i.pos.x * 108, i.pos.y * 108);
+        });
+        image.write(path.join('img', all[0].group + '.png'));
+    });
+}
 
 fs.readFile(path.join('bravefrontier_data', 'info.json'), 'utf8', (err, data) => {
 
@@ -23,18 +37,26 @@ fs.readFile(path.join('bravefrontier_data', 'info.json'), 'utf8', (err, data) =>
     }
 
     let lastGroup = -1;
+    let promises = [];
 
     for (let order_id in order) {
 
+        let unitId = order[order_id];
         let idMinus1 = order_id - 1;
 
-        let group = Math.floor(idMinus1/ 100);
+        let group = Math.floor(idMinus1 / 100);
 
-        if (group != lastGroup) {
+        if (group !== lastGroup) {
             //TODO save image
+            if (lastGroup !== -1) {
+                Promise.all(promises[lastGroup]).then(processGroup).catch((all) => {
+                    console.error(all);
+                });
+            }
             console.log('Guardant darrer grup: ', lastGroup);
             //TODO create image
             lastGroup = group;
+            promises[group] = [];
         }
 
         let rem = idMinus1 % 100;
@@ -44,6 +66,18 @@ fs.readFile(path.join('bravefrontier_data', 'info.json'), 'utf8', (err, data) =>
             y: Math.floor(rem / 10)
         };
 
+        promises[group].push(jimp.read('http://2.cdn.bravefrontier.gumi.sg/content/unit/img/unit_ills_battle_' + unitId + '.png').then((image) => {
+            return {
+                image: image,
+                pos: pos,
+                group: group
+            }
+        }).catch(error => {
+            console.error('Error on image#' + idMinus1);
+            console.error(error);
+        }));
+
+
         //TODO get and save image
         //TODO async: await all promises or whatever from a group, then save
         console.log('Afegint ', order_id, ' a ', group, ' posició: ', pos);
@@ -51,6 +85,9 @@ fs.readFile(path.join('bravefrontier_data', 'info.json'), 'utf8', (err, data) =>
 
     //TODO save image
     console.log('Acabat. Guardant darrer grup: ', lastGroup);
+    Promise.all(promises[lastGroup]).then(processGroup).catch((all) => {
+        console.error(all);
+    });
 
 
     fs.writeFile('order.json', JSON.stringify(order, null, 4), (err) => {
